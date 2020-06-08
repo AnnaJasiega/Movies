63838847fe15c06b3bb7eeb20df7da46d53ae491
Anna Jasięga <ajasiega@gmail.com>
2020-06-08 21:28:59 +0200

    some auth

--- src/app/movies/navigation/navigation.component.ts
+++ src/app/movies/navigation/navigation.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs'
+
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent {
+  name: string='anonym';
+  user$: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { 
+    this.user$ = afAuth.authState;
+  }
 
-  ngOnInit() {
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
